7702698b4ce254e15f7efdfe19a268bd5bb7f2d5
AlexSmith716 <alexsmith716@outlook.com>
2019-01-15 14:54:15 -0500

    feat(Clock component): added state alertColor.

--- client/components/widgets/Clock/Clock.js
+++ client/components/widgets/Clock/Clock.js
@@ -4,21 +4,47 @@ import React, { Component } from 'react';
 class Clock extends Component {
 
   constructor(props) {
+
     super(props);
-    this.state = {date: new Date()};
+
+    this.state = {
+      date: new Date(),
+      alertColor: this.getRandomAlert()
+    };
 
   }
 
   componentDidMount() {
-    this.timerID = setInterval( () => this.tick(), 1000 );
+    // register function to be invoked repeatedly after elapse of 1 sec
+    this.timerID = setInterval( () => this.callbackFunction(), 1000 );
   }
 
   componentWillUnmount() {
     clearInterval(this.timerID);
   }
 
-  tick() {
-    this.setState({ date: new Date() });
+  // getRandomAlert() {
+  getRandomAlert = () => {
+    const alerts = ['primary','secondary','success','danger','warning','info','light','dark'];
+    const randomAlert = alerts[ Math.floor( Math.random() * alerts.length ) ];
+    return randomAlert;
+  }
+  // getRandomColor = () => {
+  //   const characters = '0123456789ABCDEF';
+  //   const color = '#';
+  //   for (const i = 0; i < 6; i++) {
+  //     color += characters[getRandomNumber(0, 15)];
+  //   }
+  //   return color;
+  // }
+  // 
+  // getRandomNumber = (low, high) => {
+  //   const r = Math.floor(Math.random() * (high - low + 1)) + low;
+  //   return r;
+  // }
+
+  callbackFunction() {
+    this.setState({ date: new Date(), alertColor: this.getRandomAlert() });
   }
 
   render() {
@@ -27,15 +53,32 @@ class Clock extends Component {
 
     return (
 
-      <div>
-
-        <h2>Clock: state and lifecycle in a basic React component!</h2>
-
-        <hr/>
-
-        <h4>'this.state.date': { this.state.date.toLocaleTimeString() }</h4>
-
+      <div className="card text-center">
+        <h2 className="card-header">
+          Clock: state and lifecycle in a basic React component!
+        </h2>
+        <div className="card-body">
+          <div className="card-title">
+            <div className={`alert alert-${this.state.alertColor}`} role="alert">{`This is a Bootstrap v4 ${this.state.alertColor.toUpperCase()} alert`}</div>
+            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+        <div className="card-footer text-muted">
+          {this.state.date.toLocaleTimeString()}
+        </div>
       </div>
+
+      // <div>
+      //   <h2>Clock: state and lifecycle in a basic React component!</h2>
+      //   <hr/>
+      //   <blockquote className="blockquote text-center">
+      //     <h4 className="display-4">{this.state.date.toLocaleTimeString()}</h4>
+      //   </blockquote>
+      //   <blockquote className={`blockquote text-center alert alert-${this.state.alertColor}`} role="alert">
+      //     {`This is a Bootstrap v4 ${this.state.alertColor.toUpperCase()} alert—check it out!`}
+      //   </blockquote>
+      // </div>
 
     );
   }
